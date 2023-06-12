d2fd5616e38c80a5c0c4b44ced4c06685500ebd6
[name] <khandelwal2000anishka@gmail.com>
2023-06-12 12:55:06 +0530

    Problem on Array

---
+++ PrefixSum.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+ 
+void prefixSum(vector<int> &v){
+    for(int i=1;i<v.size();i++){
+        v[i]=v[i]+v[i-1];
+
+    }
+    return;
+}
+int main()
+
+{   
+    int n;
+    cin>>n;
+
+    vector<int> v;
+    for(int i=0;i<n;i++){
+        int ele;
+        cin>>ele;
+        v.push_back(ele);
+    }
+
+    prefixSum(v);
+
+    for(int i=0;i<n;i++){
+        cout<<v[i]<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
